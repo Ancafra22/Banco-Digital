dbb5c4422d7f93e487c0fee88498f714ac6fc202
Ancafra22 <andre_carlos.francisco@hotmaIl.com>
2025-10-18 20:12:33 +0100

    echo "# Banco-Digital" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/Ancafra22/Banco-Digital.git git push -u origin main
    Signed-off-by: Ancafra22 <andre_carlos.francisco@hotmaIl.com>

--- app/src/main/java/com/example/bancodigital/presenter/auth/recover/RecoverFragment.kt
+++ app/src/main/java/com/example/bancodigital/presenter/auth/recover/RecoverFragment.kt
@@ -5,8 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.example.bancodigital.databinding.FragmentRecoverBinding
+import com.example.bancodigital.util.StateView
 import com.example.bancodigital.util.initToolbar
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,6 +18,8 @@ import dagger.hilt.android.AndroidEntryPoint
 class RecoverFragment : Fragment() {
     private var _binding: FragmentRecoverBinding? = null
     private val binding get() = _binding!!
+
+    private val recoverViewModel: RecoverViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -44,10 +49,40 @@ class RecoverFragment : Fragment() {
         val email = binding.edtEmail.text.toString().trim()
 
         if (email.isNotEmpty()) {
-            Toast.makeText(requireContext(), "Login realizado com sucesso!", Toast.LENGTH_SHORT)
-                .show()
+            recoverAccount(email)
         } else {
             Toast.makeText(requireContext(), "Digite seu email", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun recoverAccount(email: String) {
+
+        recoverViewModel.recover(email).observe(viewLifecycleOwner) { stateView ->
+
+            when (stateView) {
+                is StateView.Loading -> {
+                    binding.progressBar.isVisible = true
+                }
+
+                is StateView.Sucess -> {
+                    binding.progressBar.isVisible = false
+
+                    Toast.makeText(
+                        requireContext(),
+                        "Email enviado com sucesso",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                is StateView.Error -> {
+                    binding.progressBar.isVisible = false
+                    Toast.makeText(
+                        requireContext(), "Email ou senha inválido",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
         }
     }
 
