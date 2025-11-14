a012a81733b9906312ba302f2b8eaae83310194e
Ancafra22 <andre_carlos.francisco@hotmaIl.com>
2025-11-14 10:10:09 +0000

    echo "# Banco-Digital" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/Ancafra22/Banco-Digital.git git push -u origin main
    Signed-off-by: Ancafra22 <andre_carlos.francisco@hotmaIl.com>

---
+++ app/src/main/java/com/example/bancodigital/domain/wallet/InitWalletUseCase.kt
@@ -0,0 +1,13 @@
+package com.example.bancodigital.domain.wallet
+
+import com.example.bancodigital.data.model.Wallet
+import com.example.bancodigital.data.repository.profile.wallet.WalletDataSourceImpl
+import javax.inject.Inject
+
+class InitWalletUseCase @Inject constructor(
+    private val walletDataSourceImpl: WalletDataSourceImpl
+) {
+    suspend operator fun invoke(wallet: Wallet) {
+        return walletDataSourceImpl.initWallet(wallet)
+    }
+}
