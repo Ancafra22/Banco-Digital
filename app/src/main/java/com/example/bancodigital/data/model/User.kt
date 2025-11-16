4fafea10d9c1e53495aa46ad306a47d874006286
Ancafra22 <andre_carlos.francisco@hotmaIl.com>
2025-11-16 08:28:26 +0000

    echo "# Banco-Digital" >> README.md git init git add README.md git commit -m "first commit" git branch -M main git remote add origin https://github.com/Ancafra22/Banco-Digital.git git push -u origin main
    Signed-off-by: Ancafra22 <andre_carlos.francisco@hotmaIl.com>

--- app/src/main/java/com/example/bancodigital/data/model/User.kt
+++ app/src/main/java/com/example/bancodigital/data/model/User.kt
@@ -5,6 +5,5 @@ data class User(
     val name: String = "",
     val email: String = "",
     val mobilePhone: String = "",
-    val password: String = "",
-    val confirmPassword: String = ""
+    val password: String = ""
 )
